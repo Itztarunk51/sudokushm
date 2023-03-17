3616ba8ff603205f4fb3091a841cf9c0c60aaa0f
Itztarunk51 <103349024+Itztarunk51@users.noreply.github.com>
2023-03-17 12:32:54 +0530

    Update ConnectWalletButton.js

--- src/components/ConnectWalletButton.js
+++ src/components/ConnectWalletButton.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import Web3 from "web3";
 
-function ConnectWalletButton({ web3, setAccount }) {
-  const connectWallet = async () => {
-    const accounts = await web3.eth.getAccounts();
-    setAccount(accounts[0]);
-  };
+// ...
 
-  return (
-    <div>
-      {web3 && (
-        <button onClick={connectWallet}>
-          Connect Wallet
-        </button>
-      )}
-    </div>
-  );
+async function handleConnectWallet() {
+  if (window.ethereum) {
+    try {
+      // Request account access
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const provider = new Web3(window.ethereum);
+      // Replace "YOUR_WEB3_PROVIDER_URL" with your actual Web3 provider URL
+      const web3 = new Web3("https://sphinx.shardeum.org/");
+      setWeb3(web3);
+      setAccounts(await web3.eth.getAccounts());
+      setStatus("👆🏽 Write a message in the Ethereum blockchain!");
+    } catch (error) {
+      console.error(error);
+      setStatus("❌ " + error.message);
+    }
+  } else {
+    setStatus("❌ Please install MetaMask to use this dApp!");
+  }
 }
-
-export default ConnectWalletButton;
